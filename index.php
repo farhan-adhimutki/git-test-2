925b95d0f95971edb07e9299f0430dc02db95d4d
Farhan Adhimukti Tertia <farhanfarhanadhimukti@gmail.com>
2024-09-26 14:05:38 +0700

    menambahkan paraf pada index.html

--- index.php
+++ index.php
@@ -10,6 +10,11 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium veniam fuga exercitationem eum nam eos beatae tempora harum possimus? Minima neque obcaecati sed suscipit facere numquam accusantium repellat. Nobis, enim?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>satu</li>
+    </ul>
 </body>
 
 </html>
